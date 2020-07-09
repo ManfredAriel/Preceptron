55c832123c85691ca97b06cdbee1f549ea591e1c
Manfrednet <ariel91122@gmail.com>
2020-07-08 23:36:19 -0500

    commit init

---
+++ RNA/RNA.py
@@ -0,0 +1,24 @@
+import random
+class RNA(object):
+    def __init__(self, entradas: int, salidas : int, capas_ocultas : int):      
+        self.entradas = entradas
+        self.salidas = salidas
+        self.n_ca
+
+    def printt(self):
+        print("Numero de salidas: {} Numero de salidas: {}".format(1,1))        
+    
+    class Perceptron(object):
+        def __init__(self, input : int):
+            self.input = input
+            self.W = [] 
+        def __create_pesos(self):
+            self.aux = []
+            for i in range(self.input):
+                self.aux.add(random.randrange(0, 1000, 1)/1000)
+            return
+        def __feedforward(self):
+            pass
+
+        def __feedback(self):
+            pass
---
+++ main.py
@@ -0,0 +1,3 @@
+from RNA.RNA import RNA 
+network = RNA(2,1.1)
+network.printt()
